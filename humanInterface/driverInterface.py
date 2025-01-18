f696a8a15da282d600cfc6f38d9ba958de861f2d
benwarm <160682438+benwarm@users.noreply.github.com>
2025-01-18 14:09:31 -0500

    add 90 deg buttons

--- humanInterface/driverInterface.py
+++ humanInterface/driverInterface.py
@@ -1,3 +1,6 @@
+from math import ceil
+from math import floor
+
 from wpimath import applyDeadband
 from wpimath.filter import SlewRateLimiter
 from wpilib import XboxController
@@ -6,13 +9,19 @@ from drivetrain.drivetrainPhysical import MAX_FWD_REV_SPEED_MPS,MAX_STRAFE_SPEED
 MAX_ROTATE_SPEED_RAD_PER_SEC,MAX_TRANSLATE_ACCEL_MPS2,MAX_ROTATE_ACCEL_RAD_PER_SEC_2
 from utils.allianceTransformUtils import onRed
 from utils.faults import Fault
+from utils.units import deg2Rad
+from drivetrain.drivetrainControl import DrivetrainControl
+
+
 #from utils.signalLogging import addLog
+
 
 
 class DriverInterface:
     """Class to gather input from the driver of the robot"""
 
     def __init__(self):
+        self.poseEst = DrivetrainControl().poseEst
         # contoller
         ctrlIdx = 0
         self.ctrl = XboxController(ctrlIdx)
@@ -45,13 +54,73 @@ class DriverInterface:
         #addLog("DI autoDriveToPickup", lambda: self.autoDriveToPickup, "bool")
 
     def update(self):
-        # value of contoller buttons
+        # value of controller buttons
 
         if self.ctrl.isConnected():
-            # Convert from  joystic sign/axis conventions to robot velocity conventions
+            """
+            rawGyroReading = self.poseEst.getCurEstPose().rotation().degrees()
+            goal = None
+
+            #ranges gyro from -180 - +180
+            niceGyro -= Math.floor(rawGyroReading / 360 + 0.5) * 360
+
+            # Figures what goal deg is from button pushed
+            if self.ctrl.getYButton() and niceGyro != 0:
+                goal = 0
+            elif self.ctrl.getXButton() and not 268 < niceGyro < 272:
+                goal = 270
+            elif self.ctrl.getAButton() and niceGyro != 180:
+                goal = 180
+            elif self.ctrl.getBButton() and niceGyro != 90:
+                goal = 90
+            # Figures if left or right is faster from cur rawGyroReading and goal
+            if goal is None:
+                vRotJoyRaw = self.ctrl.getRightX() * -1
+            elif abs(niceGyro - (goal + 360)) >= niceGyro - goal:
+                vRotJoyRaw = 1
+            else:
+                vRotJoyRaw = -1
+            """
+            rawGyroReadingDegs = self.poseEst.getCurEstPose().rotation().degrees()
+            goalDegs = None
+
+            if onRed():
+                if self.ctrl.getAButton():
+                    goalDegs = 0
+                elif self.ctrl.getXButton():
+                    goalDegs = -90
+                elif self.ctrl.getYButton():
+                    goalDegs = 179.9
+                elif self.ctrl.getBButton():
+                    goalDegs = 90
+            else:
+                if self.ctrl.getYButton():
+                    goalDegs = 0
+                elif self.ctrl.getBButton():
+                    goalDegs = -90
+                elif self.ctrl.getAButton():
+                    goalDegs = 179.9
+                elif self.ctrl.getXButton():
+                    goalDegs = 90
+
+            if goalDegs is None:
+                vRotJoyRaw = self.ctrl.getRightX() * -1
+            else:
+                normalizedRawGyroDegs = rawGyroReadingDegs - floor(rawGyroReadingDegs / 360 + 0.5) * 360
+                signedDistanceDegs = normalizedRawGyroDegs - goalDegs
+                normalizedDistanceDegs = signedDistanceDegs - floor(signedDistanceDegs / 360 + 0.5) * 360
+
+                if normalizedDistanceDegs<0:
+                    vRotJoyRaw = 1
+                else:
+                    vRotJoyRaw = -1
+
+                print(f"goal={goalDegs:07.1f} normalizedRawGyroDegs={normalizedRawGyroDegs:07.1f} normalizedDistanceDegs={normalizedDistanceDegs:07.1f} vRotJoyRaw={vRotJoyRaw:07.1f}")
+
+            # Convert from  joystick sign/axis conventions to robot velocity conventions
             vXJoyRaw = self.ctrl.getLeftY() * -1
             vYJoyRaw = self.ctrl.getLeftX() * -1
-            vRotJoyRaw = self.ctrl.getRightX() * -1
+            #vRotJoyRaw = self.ctrl.getRightX() * -1
 
             # Correct for alliance
             if onRed():
@@ -63,6 +132,15 @@ class DriverInterface:
             vYJoyWithDeadband = applyDeadband(vYJoyRaw, 0.15)
             vRotJoyWithDeadband = applyDeadband(vRotJoyRaw, 0.2)
 
+            useProportionalCmd = self.ctrl.getLeftBumper()
+            if useProportionalCmd:
+                if abs(normalizedDistanceDegs) > 30:
+                    rotSlowMult = 1.0
+                else:
+                    rotSlowMult = abs(normalizedDistanceDegs) / 40
+            else:
+                rotSlowMult = 0.25 if not (goalDegs is None) else 1.0
+
             # TODO - if the driver wants a slow or sprint button, add it here.
             slowMult = 1.0 if (self.ctrl.getRightBumper()) else 0.75
             #slowMult = 1.0
@@ -70,18 +148,18 @@ class DriverInterface:
             # Shape velocity command
             velCmdXRaw = vXJoyWithDeadband * MAX_STRAFE_SPEED_MPS * slowMult
             velCmdYRaw = vYJoyWithDeadband * MAX_FWD_REV_SPEED_MPS * slowMult
-            velCmdRotRaw = vRotJoyWithDeadband * MAX_ROTATE_SPEED_RAD_PER_SEC
+            velCmdRotRaw = vRotJoyWithDeadband * MAX_ROTATE_SPEED_RAD_PER_SEC * rotSlowMult
 
             # Slew rate limiter
             self.velXCmd = self.velXSlewRateLimiter.calculate(velCmdXRaw)
             self.velYCmd = self.velYSlewRateLimiter.calculate(velCmdYRaw)
             self.velTCmd = self.velTSlewRateLimiter.calculate(velCmdRotRaw)
 
-            self.gyroResetCmd = self.ctrl.getAButton()
+            #self.gyroResetCmd = self.ctrl.getAButton()
 
-            self.autoDriveToSpeaker = self.ctrl.getBButton()
-            self.autoDriveToPickup = self.ctrl.getXButton()
-            self.createDebugObstacle = self.ctrl.getYButtonPressed()
+            # self.autoDriveToSpeaker = self.ctrl.getBButton()
+            # self.autoDriveToPickup = self.ctrl.getXButton()
+            # self.createDebugObstacle = self.ctrl.getYButtonPressed()
 
             self.connectedFault.setNoFault()
 
@@ -117,3 +195,5 @@ class DriverInterface:
 
     def getCreateObstacle(self) -> bool:
         return self.createDebugObstacle
+
+    def getNormalizedDistanceDegs(goalDegs: float, rawGyroReadingDegs: float) -> float:
