4111e199f53c739d4115ccc179d2f9842716430f
spiresFrc9106 <ccrobotics@grcatholiccentral.org>
2025-03-19 14:08:01 -0400

    Log Trajectory Stuff
    Signed-off-by: spiresFrc9106 <ccrobotics@grcatholiccentral.org>

--- drivetrain/controlStrategies/trajectory.py
+++ drivetrain/controlStrategies/trajectory.py
@@ -4,20 +4,27 @@ from drivetrain.controlStrategies.holonomicDriveController import (
     HolonomicDriveController,
 )
 from jormungandr.choreo import ChoreoTrajectoryState
+from utils.signalLogging import getNowLogger
 from utils.singleton import Singleton
 
 
 class Trajectory(metaclass=Singleton):
     def __init__(self):
-        self.trajHDC = HolonomicDriveController("Trajectory")
+        self._name = "Trajectory"
+        self.trajHDC = HolonomicDriveController(self._name)
         self.curTrajCmd = None
 
+        self.curTrajCmdActiveLogger = getNowLogger(f"{self._name}/curTrajCmdActive")
     def setCmd(self, cmd: ChoreoTrajectoryState | None):
         """Send commands to the robot for motion as a part of following a trajectory
 
         Args:
             cmd (PathPlannerState): PathPlanner trajectory sample for the current time, or None for inactive.
         """
+        active = 0
+        if cmd is not None:
+            active = 1
+        self.curTrajCmdActiveLogger.logNow(active)
         self.curTrajCmd = cmd
 
     def update(self, cmdIn: DrivetrainCommand, curPose: Pose2d) -> DrivetrainCommand:
