da0da9d8c6c1b1bbbcbf3f15c4f8c9b7964f6c9b
noahbui <classicsuprememusc@gmail.com>
2025-03-28 20:29:36 -0400

    Added Blue Reef

--- AutoSequencerV2/autoSequencer.py
+++ AutoSequencerV2/autoSequencer.py
@@ -13,6 +13,9 @@ from Autonomous.modes.redCenterDriveOut import RedCenterDriveOut
 from Autonomous.modes.blueLeftDriveOut import BlueLeftDriveOut
 from Autonomous.modes.blueCenterDriveOut import BlueCenterDriveOut
 from Autonomous.modes.square import Square
+from Autonomous.modes.BlueToReefLeft import BlueToReefLeft
+from Autonomous.modes.BlueToReefCenter import BlueToReefCenter
+from Autonomous.modes.BlueToReefRight import BlueToReefRight
 
 from utils.singleton import Singleton
 from utils.allianceTransformUtils import transform
@@ -73,6 +76,10 @@ class AutoSequencer(metaclass=Singleton):
         self.mainModeList.addMode(BlueLeftDriveOut())
         self.mainModeList.addMode(BlueCenterDriveOut())
         self.mainModeList.addMode(BlueRightDriveOut())
+        self.mainModeList.addMode(BlueToReefRight())
+        self.mainModeList.addMode(BlueToReefCenter())
+        self.mainModeList.addMode(BlueToReefLeft())
+
 
     def updateMainModeListAlliance(self):
         if self._allianceChanged():
